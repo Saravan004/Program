302e58a54b871e9705383e1d22e53cde72019667
Saravan004 <46070513+Saravan004@users.noreply.github.com>
2021-09-17 09:35:59 +0530

    Update OOOPS.py

--- OOOPS.py
+++ OOOPS.py
@@ -118,6 +118,38 @@ class Ref(Shipp):
 r1 = Ref("MAE", ["dd"]
 
 
+--------------------------------
+	 #Properties
+	 
+class Employee:
+	def __init__(self, first, last):
+		self.first = first
+		self.last = last
+	@property #invoke as attribute
+	def email(self):
+		return '{}.{}@email.com'.format(self.first, self.last)
+	@property
+	def fullname(self):
+		return '{} {}'.format(self.first, self.last)
+	@fullname.setter
+	def fullname(self, name):
+		first, last = name.split(' ')
+		self.first = first
+		self.last = last
+		
+	@fullname.deleter
+	def fullname(self):
+	    self.first = None
+	    self.last = None
+emp_1 = Employee("JIm","Koya")
+emp_1.first = "John"
+#emp_1.fullname = "e ee"
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname)
+
+del emp_1.fullname #print None
+
 
 
 
